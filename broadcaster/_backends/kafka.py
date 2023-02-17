65f2b1793026587a21413716488dada18e9a00ba
Robert Philp <30417054+rnphilp@users.noreply.github.com>
2023-02-17 16:19:28 +0000

    comment out env vars

--- broadcaster/_backends/kafka.py
+++ broadcaster/_backends/kafka.py
@@ -16,14 +16,14 @@ class KafkaBackend(BroadcastBackend):
         logging.warning('inside KafkaBackend __init__')
         self._servers = [urlparse(url).netloc]
         self._consumer_channels: typing.Set = set()
-        self._security_protocol = os.environ.get("KAFKA_SECURITY_PROTOCOL") or "PLAIN"
-        self._sasl_mechanism = os.environ.get("KAFKA_SASL_MECHANISM") or "PLAIN"
-        self._sasl_plain_username = os.environ.get("KAFKA_PLAIN_USERNAME")
-        self._sasl_plain_password = os.environ.get("KAFKA_PLAIN_PASSWORD")
         logging.warning("self._servers ---> " + self._servers)
-        logging.warning("self._security_protocol --->" + self._security_protocol)
-        logging.warning("self._sasl_mechanism --->" + self._sasl_mechanism)
-        logging.warning("self._sasl_plain_username --->" + self._sasl_plain_username)
+        # self._security_protocol = os.environ.get("KAFKA_SECURITY_PROTOCOL") or "PLAIN"
+        # self._sasl_mechanism = os.environ.get("KAFKA_SASL_MECHANISM") or "PLAIN"
+        # self._sasl_plain_username = os.environ.get("KAFKA_PLAIN_USERNAME")
+        # self._sasl_plain_password = os.environ.get("KAFKA_PLAIN_PASSWORD")
+        # logging.warning("self._security_protocol --->" + self._security_protocol)
+        # logging.warning("self._sasl_mechanism --->" + self._sasl_mechanism)
+        # logging.warning("self._sasl_plain_username --->" + self._sasl_plain_username)
 
     async def connect(self) -> None:
         logging.warning('inside KafkaBackend connect()')
